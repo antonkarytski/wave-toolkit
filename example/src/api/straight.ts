1d392f12d439452e97dce38a856a9fda48c32037
antonkarytski <heyheyjude23@gmail.com>
2023-04-26 19:32:58 +0200

    fix: rm effector from deps

--- example/src/api/straight.ts
+++ example/src/api/straight.ts
@@ -9,6 +9,15 @@ simple.requestData().then((e) => {
   console.log(e);
 });
 
+const validatePhone = ApiManager.singleRequest<any, string>({
+  endpoint: `https://altekloads.com/backend/api/v2/auth/drivers/get-sms-code`,
+  method: "POST",
+  fn: (phone_number) => ({ body: { phone_number } }),
+});
+validatePhone.requestData("+48666506120").then((e) => {
+  console.log(e);
+});
+
 export const straightApi = {
   simple,
 };
